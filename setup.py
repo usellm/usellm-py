86d681d3850f1bc8abc03d13f717911c048cb367
Biraj <birajde9@gmail.com>
2023-06-06 17:14:47 +0530

    Added embed function

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 with open("README.md", "rb") as fh:
     long_description = fh.read().decode('utf-8', errors='ignore')
@@ -12,7 +12,7 @@ setup(
     description='Use Large Language Models in Python App',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    packages=['usellm'],
+    packages=find_packages(include=["usellm"], exclude=["examples"]),
     install_requires=[
         'requests>=2.31.0'
     ],
